325abd4f0ca9656a54054a080403e6d8d6c2632e
giovana1806 <132360035+giovana1806@users.noreply.github.com>
2024-02-22 15:44:37 -0300

    Delete ProjetoDTOpet directory

--- ProjetoDTOpet/src/main/java/com/projetoDTO/dto/PetDTO.java
+++
@@ -1,5 +0,0 @@
-package com.projetoDTO.dto;
-
-public record PetDTO(Long id, String nome, String nascimento, String cuidador) {
-
-}
--- ProjetoDTOpet/src/main/java/com/projetoDTO/repository/PetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projetoDTO.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projetoDTO.entities.Pet;
-
-public interface PetRepository extends JpaRepository<Pet, Long>{
-
-}
